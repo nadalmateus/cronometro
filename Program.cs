22b3a31696d7253d494a2d74ccf6abbbe530f328
Mateus Nadal <nadalmateus@hotmail.com>
2021-07-22 18:21:46 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Cronometro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+            while (currentTime < time)
+            {
+
+            }
+
+        }
+    }
+}
